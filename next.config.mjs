8912653881ebd8fdf9a54997c507744179114b10
Dyrean <fcanbeyy@gmail.com>
2025-01-05 21:40:24 +0300

    chore: update dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -7,9 +7,6 @@ const jiti = createJiti(fileURLToPath(import.meta.url));
 jiti("./src/env/server.ts");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	experimental: {
-		typedRoutes: true,
-	},
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 import { Card, CardBody } from "@nextui-org/react";
 
-export default async function Home() {
+export default function Home() {
 	return (
 		<Card className="mx-auto mt-4 max-w-md">
 			<CardBody className="text-center">
